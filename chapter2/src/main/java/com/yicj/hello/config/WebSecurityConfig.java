b8890f5175d48ecd0b94dc553c2b24189473b89f
易成杰 <626659321@qq.com>
2020-04-11 12:49:33 +0800

    保存

--- chapter2/src/main/java/com/yicj/hello/config/WebSecurityConfig.java
+++ chapter2/src/main/java/com/yicj/hello/config/WebSecurityConfig.java
@@ -1,10 +1,16 @@
 package com.yicj.hello.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -30,6 +36,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
             .csrf().disable() ;
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user").password("123").roles("USER").build());
+        manager.createUser(User.withUsername("admin").password("123").roles("USER","ADMIN").build());
+        return manager ;
+    }
+
+    /**
+     * 由于5.x版本之后默认启用了委派密码编码器,
+     * 因而按照以往的方式配置内存密码将会读取异常
+     * 所以需要暂时将密码编码器设置为NoOpPasswordEncoder
+     * @return
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance() ;
     }
 
 }
