d75480c55d4dd2f25044d10a75cff0a73b272fa6
易成杰 <626659321@qq.com>
2020-04-12 12:19:43 +0800

    保存

--- chapter04/sqls/mysql.sql
+++ chapter04/sqls/mysql.sql
@@ -1,29 +1,7 @@
---基于默认数据库模型的认证与授权
-create table users(
-    username varchar(50) not null primary key,
-    password varchar(500) not null,
-    enabled boolean not null
-);
-create table authorities (
-    username varchar(50) not null,
-    authority varchar(50) not null,
-    constraint fk_authorities_users foreign key(username) references users(username)
-);
-create unique index ix_auth_username on authorities (username,authority);
-
-insert  into users values('test','123',true) ;
-insert into authorities values ('test',"ROLE_USER");
-
----自定义数据库模型的认证与授权
-CREATE TABLE users(
-    id         BIGINT (20)   NOT NULL AUTO_INCREMENT,
-    username   VARCHAR (50)  NOT NULL ,
-    PASSWORD   VARCHAR (60)  ,
-    ENABLE     TINYINT(4)    NOT NULL DEFAULT '1' COMMENT '用户是否可用',
-    roles      TEXT          CHARACTER SET utf8 COMMENT '用户角色，多个角色之前用逗号隔开',
-    PRIMARY KEY (id),
-    KEY username (username)
+--持久化令牌表
+CREATE TABLE persistent_logins(
+    username VARCHAR(64) NOT NULL ,
+    series   VARCHAR(64) PRIMARY KEY,
+    token    VARCHAR(64) NOT NULL ,
+    last_used TIMESTAMP  NOT NULL
 ) ;
-
-INSERT INTO users(username,PASSWORD,roles) VALUES('admin','123','ROLE_USER,ROLE_ADMIN') ;
-INSERT INTO users(username,PASSWORD,roles) VALUES('user','123','ROLE_USER') ;
--- chapter04/src/main/java/com/yicj/hello/config/WebSecurityConfig.java
+++ chapter04/src/main/java/com/yicj/hello/config/WebSecurityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+
+import javax.sql.DataSource;
 
 
 @EnableWebSecurity
@@ -13,9 +16,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private MyUserDetailService userDetailService ;
+    @Autowired
+    private DataSource dataSource ;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl() ;
+        jdbcTokenRepository.setDataSource(dataSource);
         http
         .authorizeRequests()
             .antMatchers("/admin/api/**").hasRole("ADMIN")
@@ -28,9 +35,32 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .permitAll()
             .and()
          //增加自动登录功能，默认为散列加密
+//        .rememberMe()
+//            .userDetailsService(userDetailService)
+//            .key("blurooo");
         .rememberMe()
-            .userDetailsService(userDetailService)
-            .key("blurooo");
+              .userDetailsService(userDetailService)
+              .tokenRepository(jdbcTokenRepository)
+              .and()
+        .logout()
+        //指定接受注销请求的路由
+        .logoutUrl("/myLogout")
+        //注销成功，重定向到该路径下
+        .logoutSuccessUrl("/")
+        //注销成功处理方式，不同于logoutSuccessUrl的重定向，logoutSuccessHandler更加灵活
+        .logoutSuccessHandler((request,response,authentication) ->{
+            System.out.println("注销成功.."); //具体注销成功执行的逻辑
+        })
+        //使该用户的HttpSession失效
+        .invalidateHttpSession(true)
+        //注销成功，删除指定cookie
+        .deleteCookies("cookie1","cookie2")
+        //用户注销的处理句柄，允许自定义一些清理策略
+        //事实上LogoutSuccessHandler也能做到
+        .addLogoutHandler( (request,response,authentication) ->{
+            //注销业务
+        })
+        ;
     }
 
 }
